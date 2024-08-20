5cf90b2356a962212944db319ff7620a5f3d2daa
Mark Krutzler <mk@Marks-MacBook-Air.local>
2024-08-20 08:48:55 +0200

    added a reminer to use orgmode and conver to latex

--- handout/.auctex-auto/article.el
+++ handout/.auctex-auto/article.el
@@ -7,6 +7,13 @@
                      '(("article" "")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("titlesec" "") ("titling" "") ("hyperref" "") ("geometry" "margin=1in") ("ulem" "normalem") ("xcolor" "") ("graphicx" "")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "art10"
